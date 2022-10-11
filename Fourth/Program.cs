0375e1c5761346ba090c07314decb84a64c970ba
ViktoriaKukh <bstet@mail.ru>
2022-10-11 14:52:59 +0500

    Fourth exercise without repeat

--- Fourth/Program.cs
+++ Fourth/Program.cs
@@ -1,16 +1,35 @@
 ﻿Console.WriteLine("Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.");
 Console.WriteLine("Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.");
 
+
+
 void FillCubicMatrix (int[,,] array)
-{
+{   
+    int[] arrayOfAlreadyThere = new int[100];
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             for (int k = 0; k < array.GetLength(2); k++)
             {
-                array[i,j,k] = new Random().Next(1,10);
+                int randomNumber = new Random().Next(10,100);
+                while (arrayOfAlreadyThere[randomNumber] > 0)
+                {
+                   randomNumber = new Random().Next(10,100); 
+                }
+                array[i,j,k] = randomNumber;
+                arrayOfAlreadyThere[randomNumber] = 1;
             } 
+        }
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+
+            }
         }
     }
 }
@@ -32,9 +51,9 @@ void PrintCubicMatrix (int[,,] array)
     }
 }
 
-int widthMatrix = 4;
-int heightMatrix = 4;
-int depthMatrix = 4;
+int widthMatrix = 3;
+int heightMatrix = 3;
+int depthMatrix = 3;
 int [,,] cubicMatrix = new int[heightMatrix,widthMatrix,depthMatrix];
 FillCubicMatrix(cubicMatrix);
 PrintCubicMatrix(cubicMatrix);
